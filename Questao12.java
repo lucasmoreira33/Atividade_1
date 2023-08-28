5e631d8b75764ea0a299b0d4b17325e8d518287f
lucasmoreira33 <131310902+lucasmoreira33@users.noreply.github.com>
2023-08-28 17:28:33 -0300

    Add files via upload

---
+++ Questao12.java
@@ -0,0 +1,9 @@
+public class Idade12 {
+    public static void main(String[] args) {
+        int idadeAnos = 18;
+
+        int idadeMeses = idadeAnos * 12;
+
+        System.out.println("Sua idade em meses é: " + idadeMeses);
+    }
+}
